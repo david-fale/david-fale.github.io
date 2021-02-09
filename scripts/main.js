ebfcd23606659e1351ee9fb2aaeed3ec4c7ecf8c
{ <david64th@gmail.com>
2021-02-10 09:01:12 +1300

    added nav bar

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+function navMenu () {
+    let x = document.getElementById("local-links");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
