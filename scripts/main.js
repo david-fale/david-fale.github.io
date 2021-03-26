329ff230d9746c0fbdba7df353cee18c1cf88c48
{ <david64th@gmail.com>
2021-03-27 02:11:05 +1300

    updated nav

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,4 @@
 function navMenu() {
-    let y = document.getElementById("change");
     let x = document.getElementById("local-links");
     if (x.style.display === "block") {
         x.style.display = "none";
